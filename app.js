ecc335cbdd2d6ca6989b71d8927c9b4f106dc070
nemogoc <1668286+nemogoc@users.noreply.github.com>
2018-03-11 21:57:04 -0500

    add initial routes for index and campgrounds pages

--- app.js
+++ app.js
@@ -1,3 +1,36 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 
+app.use(express.static("public"));
 app.set("view engine", "ejs");
+
+
+
+app.get("/", function(req, res){
+  res.render("index");
+});
+
+app.get("/campgrounds", function(req, res){
+  var campgrounds = [
+    {
+      name: "Yellowstone Lake",
+      image: "/resources/img/548022.png"
+    },
+    {
+      name: "New Glarus Woods",
+      image: "/resources/img/839807.png"
+    },
+    {
+      name: "Governor Dodge",
+      image: "/resources/img/1845906.png"
+    }
+
+  ];
+
+  res.render("campgrounds", {campgrounds: campgrounds} );
+});
+
+
+app.listen(3000, function(){
+  console.log("serving on port 3000");
+});
