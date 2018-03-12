112de23a58031122aecb9251a63a5acb3ad978af
nemogoc <1668286+nemogoc@users.noreply.github.com>
2018-03-12 07:27:25 -0500

    implement add campground routes and page

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+
+var campgrounds = [
+  {
+    name: "Yellowstone Lake",
+    image: "/resources/img/548022.png"
+  },
+  {
+    name: "New Glarus Woods",
+    image: "/resources/img/839807.png"
+  },
+  {
+    name: "Governor Dodge",
+    image: "/resources/img/1845906.png"
+  }
+];
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 
@@ -10,24 +27,21 @@ app.get("/", function(req, res){
   res.render("index");
 });
 
+app.post("/campgrounds", function(req, res){
+  campgrounds.push({
+    name: req.body.name,
+    image: req.body.url
+  });
+
+  res.redirect("/campgrounds");
+});
+
 app.get("/campgrounds", function(req, res){
-  var campgrounds = [
-    {
-      name: "Yellowstone Lake",
-      image: "/resources/img/548022.png"
-    },
-    {
-      name: "New Glarus Woods",
-      image: "/resources/img/839807.png"
-    },
-    {
-      name: "Governor Dodge",
-      image: "/resources/img/1845906.png"
-    }
-
-  ];
-
   res.render("campgrounds", {campgrounds: campgrounds} );
+});
+
+app.get("/campgrounds/new", function(req, res){
+  res.render("new");
 });
 
 
