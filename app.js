32981a89c7a389f5d96bf175d3a3009d864dbabf
nemogoc <1668286+nemogoc@users.noreply.github.com>
2018-03-14 20:41:23 -0500

    add show route & page

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ mongoose.connect("mongodb://localhost/yelpcamp", function(err, body){
 
 var campgroundSchema = new mongoose.Schema({
   name: String,
-  image: String
+  image: String,
+  description: String
 });
 
 var Campground = mongoose.model("Campground", campgroundSchema);
@@ -25,13 +26,7 @@ app.get("/", function(req, res){
   res.render("index");
 });
 
-app.post("/campgrounds", function(req, res){
-  createCampground(req.body.name, req.body.url);
-  //technically a race condition here, new campground may not render
-
-  res.redirect("/campgrounds");
-});
-
+//INDEX route - Displays list of all campgrounds
 app.get("/campgrounds", function(req, res){
   Campground.find({}, function(err, campgrounds){
     if (err){
@@ -43,8 +38,29 @@ app.get("/campgrounds", function(req, res){
   });
 });
 
+//NEW route - Displays form to make new campground
 app.get("/campgrounds/new", function(req, res){
   res.render("new");
+});
+
+//CREATE route - Adds new campground (from NEW form) to DB
+app.post("/campgrounds", function(req, res){
+  createCampground(req.body.name, req.body.url, req.body.description);
+  //technically a race condition here, new campground may not render
+
+  res.redirect("/campgrounds");
+});
+
+//SHOW route - show more detail about one specific campground
+app.get("/campgrounds/:id", function(req, res){
+  Campground.findById(req.params.id, function(err, campground){
+    if (err){
+      console.log(error);
+    }
+    else {
+      res.render("details", { campground: campground });
+    }
+  });
 });
 
 
@@ -54,31 +70,10 @@ app.listen(3000, function(){
 
 
 //doesn't block, may introduce race condition
-function createCampground(name, image) {
-  Campground.create({ name: name, image: image }, function (err, ret) {
+function createCampground(name, image, description) {
+  Campground.create({ name: name, image: image, description: description }, function (err, ret) {
     if (err) {
       console.log("Error adding campground to db: " + err);
-    }
-  });
-}
-
-//This doesn't work yet because it doesn't block the render.
-function getCampgrounds(){
-  var campgrounds = [];
-  Campground.find({}, function(err, ret){
-    if (err){
-      console.log("Error getting campgrounds from db: " + err);
-    }
-    else {
-      console.log(ret);
-      ret.forEach(function(campgr){
-        campgrounds.push({
-          name: campgr.name,
-          image: campgr.image
-        })
-      });
-      console.log("In call: " + campgrounds);
-      return campgrounds;
     }
   });
 }
