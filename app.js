a71fd1dd1ff281a29438dbfb5259b1667cbf893b
nemogoc <1668286+nemogoc@users.noreply.github.com>
2018-03-22 19:22:53 -0500

    move models out of app.js & add comments

--- app.js
+++ app.js
@@ -1,50 +1,47 @@
 var express = require('express'),
-    app = express(),
-    mongoose = require('mongoose'),
-    bodyParser = require('body-parser');
+  app = express(),
+  mongoose = require('mongoose'),
+  bodyParser = require('body-parser'),
+  Campground = require('./models/campground');
+Comment = require('./models/comment');
+seedDb = require('./seeds');
 
-mongoose.connect("mongodb://localhost/yelpcamp", function(err, body){
-  if(err){
+mongoose.connect("mongodb://localhost/yelpcamp", function (err, body) {
+  if (err) {
     console.log("Error connecting: " + err);
   }
 });
 
-var campgroundSchema = new mongoose.Schema({
-  name: String,
-  image: String,
-  description: String
-});
-
-var Campground = mongoose.model("Campground", campgroundSchema);
+seedDb();
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 
 
-app.get("/", function(req, res){
+app.get("/", function (req, res) {
   res.render("home");
 });
 
 //INDEX route - Displays list of all campgrounds
-app.get("/campgrounds", function(req, res){
-  Campground.find({}, function(err, campgrounds){
-    if (err){
+app.get("/campgrounds", function (req, res) {
+  Campground.find({}, function (err, campgrounds) {
+    if (err) {
       console.log("Error getting campgrounds from db: " + err);
     }
     else {
-      res.render("index", {campgrounds: campgrounds} );
+      res.render("index", {campgrounds: campgrounds});
     }
   });
 });
 
 //NEW route - Displays form to make new campground
-app.get("/campgrounds/new", function(req, res){
+app.get("/campgrounds/new", function (req, res) {
   res.render("new");
 });
 
 //CREATE route - Adds new campground (from NEW form) to DB
-app.post("/campgrounds", function(req, res){
+app.post("/campgrounds", function (req, res) {
   createCampground(req.body.name, req.body.url, req.body.description);
   //technically a race condition here, new campground may not render
 
@@ -52,26 +49,26 @@ app.post("/campgrounds", function(req, res){
 });
 
 //SHOW route - show more detail about one specific campground
-app.get("/campgrounds/:id", function(req, res){
-  Campground.findById(req.params.id, function(err, campground){
-    if (err){
-      console.log(error);
+app.get("/campgrounds/:id", function (req, res) {
+  Campground.findById(req.params.id).populate("comments").exec(function (err, campground) {
+    if (err) {
+      console.log(err);
     }
     else {
-      res.render("show", { campground: campground });
+      res.render("show", {campground: campground});
     }
   });
 });
 
 
-app.listen(3000, function(){
+app.listen(3000, function () {
   console.log("serving on port 3000");
 });
 
 
 //doesn't block, may introduce race condition
 function createCampground(name, image, description) {
-  Campground.create({ name: name, image: image, description: description }, function (err, ret) {
+  Campground.create({name: name, image: image, description: description}, function (err, ret) {
     if (err) {
       console.log("Error adding campground to db: " + err);
     }
