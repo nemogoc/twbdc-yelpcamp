5fbb13741d7ce7e285024d71e825f0f1ae714be8
nemogoc <1668286+nemogoc@users.noreply.github.com>
2018-03-28 14:34:01 -0500

    add comment edit/del routes/view/auth middleware

--- routes/comments.js
+++ routes/comments.js
@@ -35,11 +35,65 @@ router.post("/", isLoggedIn, function (req, res) {
   });
 });
 
+//EDIT
+router.get("/:commentId/edit", checkCommentOwnership, function(req, res){
+  Comment.findById(req.params.commentId, function(err, comment){
+    if (err){
+      console.log(err);
+      return res.redirect("/campgrounds");
+    }
+    res.render("comments/edit", {comment: comment, campgroundId: req.params.id});
+  });
+});
+
+//UPDATE
+router.put("/:commentId", checkCommentOwnership, function(req, res){
+  Comment.findByIdAndUpdate(req.params.commentId, req.body.comment, function(err, comment){
+    if (err){
+      console.log(err);
+    }
+    res.redirect("/campgrounds/" + req.params.id);
+  });
+});
+
+//DESTROY
+router.delete("/:commentId", checkCommentOwnership, function(req, res){
+  Comment.findByIdAndRemove(req.params.commentId, function(err){
+    if(err){
+      console.log(err);
+    }
+    res.redirect("/campgrounds/" + req.params.id);
+  });
+});
+
 function isLoggedIn(req, res, next){
   if(req.isAuthenticated()){
     return next();
   }
   res.redirect("/login");
+}
+
+function checkCommentOwnership(req, res, next){
+  //is user logged in
+  if(req.isAuthenticated()){
+    Comment.findById(req.params.commentId, function(err, comment){
+      if(err){
+        res.redirect("back");
+      }
+      else{
+        //is logged in user the creator
+        if(comment.author.id.equals(req.user._id)) {
+          return next();
+        }
+        else {
+          res.redirect("back");
+        }
+      }
+    });
+  }
+  else {
+    res.redirect("back");
+  }
 }
 
 module.exports = router;
